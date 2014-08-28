f88d5f206b01a3127421cd283246f02b91944b72
David McClure <davidwilliammcclure@gmail.com>
2014-08-28 14:52:47 -0700

    Rigs up a gulp clean task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,44 @@
 
 
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var webserver = require('gulp-webserver');
 var jade = require('gulp-jade');
+var less = require('gulp-less');
+
+gulp.task('clean', function() {
+  gulp.src('_site', { read: false })
+    .pipe(clean());
+});
 
 gulp.task('template', function() {
   gulp.src('index.jade')
     .pipe(jade())
-    .pipe(gulp.dest('_site/'))
+    .pipe(gulp.dest('_site'))
+});
+
+gulp.task('less', function() {
+  gulp.src('assets/stylesheets/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('_site'))
 });
 
 gulp.task('server', function() {
   gulp.src('_site')
     .pipe(webserver({
-      livereload: true,
-      open: true
+      livereload: true
     }));
 });
 
 gulp.task('watch', function() {
   gulp.watch('index.jade', ['template']);
+  gulp.watch('assets/stylesheets/*', ['less']);
 });
 
 gulp.task('default', [
-  'watch',
+  'clean',
   'template',
+  'less',
+  'watch',
   'server'
 ]);
