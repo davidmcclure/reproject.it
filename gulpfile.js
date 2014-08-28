9eedd7c82b6b920f6618bdea611c95b8636c14eb
David McClure <davidwilliammcclure@gmail.com>
2014-08-28 14:02:03 -0700

    Adds gulp watch and default tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,17 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 
-
 gulp.task('template', function() {
   gulp.src('index.jade')
     .pipe(jade())
     .pipe(gulp.dest('_site/'))
 });
+
+gulp.task('watch', function() {
+  gulp.watch('index.jade', ['template']);
+});
+
+gulp.task('default', [
+  'watch',
+  'template'
+]);
